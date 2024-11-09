c895262cb2e6c44ec29ddaa99217907889349836
Sanka <sankasanjeevadg@gmail.com>
2024-11-09 10:14:26 +0530

    💄 Add animations to header when scrolling the screen

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,6 +7,7 @@ import { cn } from '@/lib/utils';
 import { Button } from '../ui/button';
 import { LogoIcon } from '../../../public/icons';
 import CenteredContainer from '../centered-container';
+import { useElementIsVisible } from '@/hook';
 
 const navLinks = [
   { label: 'Home', path: '/' },
@@ -18,6 +19,8 @@ const navLinks = [
 export default function Header() {
   const pathname = usePathname();
 
+  const { ref, visible } = useElementIsVisible({ initialVisible: true });
+
   const isActiveLink = (path: string) => {
     if (path === '/') {
       return pathname === '/';
@@ -26,32 +29,41 @@ export default function Header() {
   };
 
   return (
-    <header className="fixed top-0 w-full text-white">
-      <CenteredContainer className="flex h-20 items-center justify-between">
-        <div className="flex items-center gap-2 text-xl font-semibold">
-          <LogoIcon />
-          <span>Hire Hub</span>
-        </div>
-        <nav>
-          <ul className="flex gap-5">
-            {navLinks.map(({ label, path }) => (
-              <li
-                key={path}
-                className={cn(
-                  'px-3 py-2 text-white/50 hover:text-white/80',
-                  isActiveLink(path) && 'text-white'
-                )}
-              >
-                <Link href={path}>{label}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="flex gap-1">
-          <Button variant="ghost">Login</Button>
-          <Button>Register</Button>
-        </div>
-      </CenteredContainer>
-    </header>
+    <>
+      <div ref={ref} className="absolute left-0 top-0 h-20" />
+
+      <header
+        className={cn(
+          'supports-backdrop-blur:bg-slate/60 fixed top-0 w-full flex-none py-5 text-white backdrop-blur transition-all duration-500',
+          !visible && 'bg-black/70 py-2'
+        )}
+      >
+        <CenteredContainer className="flex items-center justify-between">
+          <div className="flex items-center gap-2 text-xl font-semibold">
+            <LogoIcon />
+            <span>Hire Hub</span>
+          </div>
+          <nav>
+            <ul className="flex gap-5">
+              {navLinks.map(({ label, path }) => (
+                <li
+                  key={path}
+                  className={cn(
+                    'px-3 py-2 text-white/50 hover:text-white/80',
+                    isActiveLink(path) && 'text-white'
+                  )}
+                >
+                  <Link href={path}>{label}</Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="flex gap-1">
+            <Button variant="ghost">Login</Button>
+            <Button>Register</Button>
+          </div>
+        </CenteredContainer>
+      </header>
+    </>
   );
 }
