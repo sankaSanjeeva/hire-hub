c9e22d706b07d1a8ff79a66b9dffefedb15d1d1b
Sanka <sankasanjeevadg@gmail.com>
2024-12-01 19:00:04 +0530

    👷 API integrations of top companies

--- src/app/jobs/_components/top-companies/index.tsx
+++ src/app/jobs/_components/top-companies/index.tsx
@@ -1,8 +1,21 @@
+import Image from 'next/image';
 import { CenteredContainer, SectionHeader } from '@/components';
 import { Badge } from '@/components/ui/badge';
-import Image from 'next/image';
+import prisma from '@/lib/db';
 
-export default function TopCompanies() {
+export default async function TopCompanies() {
+  const companies = await prisma.company.findMany({
+    include: {
+      _count: true,
+    },
+    orderBy: {
+      jobs: {
+        _count: 'desc',
+      },
+    },
+    take: 4,
+  });
+
   return (
     <section className="bg-theme/10">
       <CenteredContainer className="pb-28 pt-14">
@@ -13,26 +26,27 @@ export default function TopCompanies() {
         />
 
         <div className="mt-14 grid grid-cols-[300px] justify-center gap-6 sm:grid-cols-[repeat(2,300px)] xl:grid-cols-[repeat(4,300px)]">
-          {[1, 2, 3, 4].map((company) => (
+          {companies.map((company) => (
             <div
-              key={company}
+              key={company.id}
               className="flex flex-col items-center gap-8 rounded-3xl bg-white px-10 py-12 shadow-card"
             >
               <div className="flex flex-col items-center gap-4">
                 <Image
-                  src="https://i.pravatar.cc/150?img=67"
+                  src={company.logo ?? '/icons/image-not-found.svg'}
                   width={60}
                   height={60}
                   alt="avatar-1"
                   className="rounded-full"
                 />
-                <span className="text-2xl font-semibold">Tesla</span>
+                <span className="line-clamp-1 text-center text-2xl font-semibold">
+                  {company.name}
+                </span>
               </div>
-              <p className="line-clamp-3 text-center text-black/80">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Numquam quidem
+              <p className="line-clamp-3 min-h-[72px] text-center text-black/80">
+                {company.description}
               </p>
-              <Badge>8 open jobs</Badge>
+              <Badge>{company._count.jobs} open jobs</Badge>
             </div>
           ))}
         </div>
